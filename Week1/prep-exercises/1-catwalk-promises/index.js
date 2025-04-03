7066b6001b2b0990f2d4c038e57fd975e88ad0d6
Yana Seniuk <senyukyana26@gmail.com>
2025-04-03 15:03:01 +0200

    Solved prep 1 week API

--- Week1/prep-exercises/1-catwalk-promises/index.js
+++ Week1/prep-exercises/1-catwalk-promises/index.js
@@ -5,36 +5,42 @@ const STEP_INTERVAL_MS = 50;
 const DANCE_TIME_MS = 5000;
 const DANCING_CAT_URL =
   'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+const WALKING_CAT_URL =
+  'http://www.anniemation.com/clip_art/images/cat-walk.gif';
 
 function walk(img, startPos, stopPos) {
-  return new Promise((resolve) => {
-    // Resolve this promise when the cat (`img`) has walked from `startPos` to
-    // `stopPos`.
-    // Make good use of the `STEP_INTERVAL_PX` and `STEP_INTERVAL_MS`
-    // constants.
+  return new Promise(resolve => {
+    const timerId = setInterval(() => {
+      img.style.left = `${startPos}px`;
+      startPos += STEP_SIZE_PX;
+      if (startPos >= stopPos) {
+        img.style.left = `${stopPos}px`;
+        clearInterval(timerId);
+        resolve();
+      }
+    }, STEP_INTERVAL_MS);
   });
 }
 
 function dance(img) {
-  return new Promise((resolve) => {
-    // Switch the `.src` of the `img` from the walking cat to the dancing cat
-    // and, after a timeout, reset the `img` back to the walking cat. Then
-    // resolve the promise.
-    // Make good use of the `DANCING_CAT_URL` and `DANCE_TIME_MS` constants.
+  return new Promise(resolve => {
+    img.src = DANCING_CAT_URL;
+    setTimeout(() => {
+      img.src = WALKING_CAT_URL;
+      resolve();
+    }, DANCE_TIME_MS);
   });
 }
 
 function catWalk() {
   const img = document.querySelector('img');
-  const startPos = -img.width;
+  const startPos = 0;
   const centerPos = (window.innerWidth - img.width) / 2;
-  const stopPos = window.innerWidth;
-
-  // Use the `walk()` and `dance()` functions to let the cat do the following:
-  // 1. Walk from `startPos` to `centerPos`.
-  // 2. Then dance for 5 secs.
-  // 3. Then walk from `centerPos` to `stopPos`.
-  // 4. Repeat the first three steps indefinitely.
+  const stopPos = window.innerWidth - img.clientWidth;
+  walk(img, startPos, centerPos)
+    .then(() => dance(img))
+    .then(() => walk(img, centerPos, stopPos))
+    .then(catWalk);
 }
 
 window.addEventListener('load', catWalk);
